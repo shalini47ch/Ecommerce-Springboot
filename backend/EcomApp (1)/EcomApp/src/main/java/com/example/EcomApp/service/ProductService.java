8528cb32558b3916a4507eec48953643afb887f0
shalini47ch <shalini47choudhary@gmail.com>
2025-05-24 21:44:50 +0530

    Initial commit with frontend and backend

---
+++ backend/EcomApp (1)/EcomApp/src/main/java/com/example/EcomApp/service/ProductService.java
@@ -0,0 +1,46 @@
+package com.example.EcomApp.service;
+
+import com.example.EcomApp.model.Product;
+import com.example.EcomApp.repository.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+public class ProductService {
+    @Autowired
+    ProductRepo repo;
+
+    public List<Product> getAllProducts(){
+        return repo.findAll();
+    }
+    public Product getProductById(int id) {
+        return repo.findById(id).orElse(new Product());
+
+    }
+    public Product addProduct(Product product, MultipartFile imageFile) throws IOException {
+        //since we have declared the fields like imageName,imageType and imageData
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        product.setImageData(imageFile.getBytes());
+        return repo.save(product);
+    }
+
+    public Product updateProduct(int id, Product product, MultipartFile imageFile) throws IOException {
+
+        //here write the logic for updateProduct
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        product.setImageData(imageFile.getBytes());
+        return repo.save(product);
+
+    }
+
+    public void deleteProduct(int id) {
+        repo.deleteById(id);
+    }
+}
+
---
+++ backend/EcomApp (1)/EcomApp/src/test/java/com/example/EcomApp/EcomAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EcomApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
